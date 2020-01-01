4d198aee2a0169291ba48d3a1eb5480a614df6b4
chen.levin <chen.levin@ironsrc.com>
2020-01-01 18:56:02 +0200

    first commit

--- src/app/pages/actions/actions.module.ts
+++ src/app/pages/actions/actions.module.ts
@@ -3,17 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ActionsComponent } from './actions.component';
 import {Routes, RouterModule} from '@angular/router';
 import {TodoListModule} from '../../components/todo-list/todo-list.module';
-import {ActionsResolverService} from './actions-resolver.service';
-import {ActionsGuard} from './actions.guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: ActionsComponent,
-    // resolve: {
-    //   resolver: ActionsResolverService
-    // },
-    // canActivate: [ActionsGuard]
   }
 ]
 
