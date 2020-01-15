5e2898194697be9c5aa8185a64b8d7c286fca42d
chen.levin <chen.levin@ironsrc.com>
2020-01-15 17:30:35 +0200

    first commit

--- src/app/pages/actions/actions.component.ts
+++ src/app/pages/actions/actions.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy, OnDestroy} from '@angular/core';
 import {TodoItem} from '../../entities/todo-item';
 import {TodoListService} from '../../services/todo-list.service';
 import {ActivatedRoute} from '@angular/router';
 import {FormControl, Validators} from '@angular/forms';
+import {takeUntil, scan} from 'rxjs/operators';
+import {Subject, Observable, merge} from 'rxjs';
 
 @Component({
   selector: 'app-actions',
@@ -10,23 +12,55 @@ import {FormControl, Validators} from '@angular/forms';
   styleUrls: ['./actions.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ActionsComponent implements OnInit {
+export class ActionsComponent implements OnInit, OnDestroy {
 
   todoList: TodoItem[];
+  // todoList$: Observable<TodoItem[]>;
   userName: string;
   selectedActions = new FormControl(null, Validators.required);
+
+  private onDestroy$ = new Subject();
+  // private todoListSubject$ = new Subject<TodoItem>();
 
   constructor(private todoListService: TodoListService, private route: ActivatedRoute) {}
 
   ngOnInit() {
+
     this.todoList = this.todoListService.getTodoListData();
+
+    // this.todoList$ = merge(this.todoListService.getTodoListData(), this.todoListSubject$.asObservable())
+    //   .pipe(
+    //     scan((acc, value) => {
+    //       if (Array.isArray(value)) {
+    //         return [...acc, ...value];
+    //       }
+    //       return [...acc, value];
+    //     }, [])
+    //   );
 
     this.selectedActions.setValue(this.todoListService.getSelectedActions());
 
-    this.selectedActions.valueChanges.subscribe(value => {
-      this.todoListService.updateSelectedActions(value);
-    });
+    // this.selectedActions.valueChanges.subscribe(value => {
+    //   this.todoListService.updateSelectedActions(value);
+    // });
+
+    this.selectedActions.valueChanges
+      .pipe(
+        takeUntil(this.onDestroy$)
+      )
+      .subscribe(value => {
+        this.todoListService.updateSelectedActions(value);
+      });
   }
+
+  ngOnDestroy() {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
+
+  // addItem(item: TodoItem): void {
+  //   this.todoListSubject$.next(item);
+  // }
 
   addItem(item: TodoItem): void {
     this.todoList = [...this.todoList, item]; // Immutable
