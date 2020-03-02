53bdf380336831a6942bd9c01877910a6d11805f
chen.levin <chen.levin@ironsrc.com>
2020-03-02 19:27:21 +0200

    add todo marks

--- src/app/directives/tooltip/tooltip.directive.ts
+++ src/app/directives/tooltip/tooltip.directive.ts
@@ -7,6 +7,7 @@ export class TooltipDirective implements AfterViewInit {
 
   constructor(private elementRef: ElementRef) {}
 
+  // Todo
   ngAfterViewInit() {
     console.log(`Element has rendered to DOM => ${this.elementRef.nativeElement.innerText}`);
     // if (this.elementRef.nativeElement.scrollWidth > this.elementRef.nativeElement.clientWidth) {
