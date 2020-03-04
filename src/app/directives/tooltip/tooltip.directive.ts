d1e35c5bf575d1abc3d4c1eb02d45a2867256caa
chen.levin <chen.levin@ironsrc.com>
2020-03-04 12:02:53 +0200

    remove comments

--- src/app/directives/tooltip/tooltip.directive.ts
+++ src/app/directives/tooltip/tooltip.directive.ts
@@ -7,12 +7,10 @@ export class TooltipDirective implements AfterViewInit {
 
   constructor(private elementRef: ElementRef) {}
 
-  // Todo
   ngAfterViewInit() {
-    console.log(`Element has rendered to DOM => ${this.elementRef.nativeElement.innerText}`);
-    // if (this.elementRef.nativeElement.scrollWidth > this.elementRef.nativeElement.clientWidth) {
-    //   this.elementRef.nativeElement.setAttribute('title', this.elementRef.nativeElement.innerText);
-    // }
+    if (this.elementRef.nativeElement.scrollWidth > this.elementRef.nativeElement.clientWidth) {
+      this.elementRef.nativeElement.setAttribute('title', this.elementRef.nativeElement.innerText);
+    }
   }
 
 }
