a3056db82093a3ee33e74441ad2fde69d41e3d0c
chen.levin <chen.levin@ironsrc.com>
2020-09-29 13:03:46 +0300

    add soultion for home ex

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {TodoItem} from '../../entities/todo-item';
 import {TodoListService} from '../../services/todo-list.service';
 
+const ENTER_KEY_CODE = 13;
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -12,6 +14,7 @@ export class TodoListComponent implements OnInit {
   list: TodoItem[];
 
   todoActionName = '';
+  showErrorMessage = false;
 
   constructor(private todoListService: TodoListService) {}
 
@@ -19,11 +22,22 @@ export class TodoListComponent implements OnInit {
     this.list = this.todoListService.getTodoListData();
   }
 
-  // Todo
-  add() {
-    if (this.todoActionName) {
+  add(): void {
+    if (!this.todoActionName) {
+      return;
+    }
+    if (this.list.some(item => item.title.toLowerCase() === this.todoActionName.toLowerCase())) {
+      this.showErrorMessage = true;
+    } else {
       this.list.push({id: this.list.length, title: this.todoActionName});
       this.todoActionName = '';
+    }
+  }
+
+  onKeyDown($event): void {
+    this.showErrorMessage = false;
+    if ($event.keyCode === ENTER_KEY_CODE) {
+      this.add();
     }
   }
 
