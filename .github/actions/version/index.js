fb9dc03300fb9a2b1bd55afba57a07069ddaeba7
chen.levin <chen.levin@ironsrc.com>
2020-10-08 18:33:32 +0300

    fied type

--- .github/actions/version/index.js
+++ .github/actions/version/index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 
 try {
   const bucketTagsContent = core.getInput('bucketTagsContent');
+  console.log(bucketTagsContent);
   const bucketTags = JSON.parse(bucketTagsContent);
   const version = bucketTags.TagSet.find(curr => curr.Key === 'version').Value;
 
