01b9fcde80ea073e5249c828b0b2806aa8cd82f2
chen.levin <chen.levin@ironsrc.com>
2020-10-08 18:36:36 +0300

    replace duplicate name

--- .github/actions/version/index.js
+++ .github/actions/version/index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 
 try {
-  const bucketTagsContent = core.getInput('bucketTagsContent');
+  const bucketTagsContent = core.getInput('bucketTagsJson');
   console.log(bucketTagsContent);
   const bucketTags = JSON.parse(bucketTagsContent);
   const version = bucketTags.TagSet.find(curr => curr.Key === 'version').Value;
